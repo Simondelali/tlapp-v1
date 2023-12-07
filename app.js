f0d2956893ee9e586e0f6cdf1cf3dd95af380e2f
Simon Atiegar <atiegardelali@gmail.com>
2023-12-07 19:45:27 +0000

    setup express server

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.get("/", function(req,res){
+    res.send("Hello, world")
+})
+
+
+
+
+app.listen(3000, function(){
+    console.log("Server is cooking on port 3000");
+})
+
 
